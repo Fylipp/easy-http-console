837003c0effc0783bcda9b565b8747f376d8782a
Philipp Ploder <philipp.ploder@outlook.com>
2017-02-14 00:32:53 +0100

    Renamed handlers to listeners

--- src/main/java/com/pploder/ehc/CommandListener.java
+++ src/main/java/com/pploder/ehc/CommandListener.java
@@ -3,13 +3,13 @@ package com.pploder.ehc;
 import java.util.function.Consumer;
 
 /**
- * A function that handles a {@link Message}.
+ * A function that handles a {@link Command}.
  *
  * @author Philipp Ploder
  * @version 1.0.0
  * @since 1.0.0
  */
 @FunctionalInterface
-public interface MessageHandler extends Consumer<Message> {
+public interface CommandListener extends Consumer<Command> {
 
 }
--- src/main/java/com/pploder/ehc/MessageListener.java
+++ src/main/java/com/pploder/ehc/MessageListener.java
@@ -3,13 +3,13 @@ package com.pploder.ehc;
 import java.util.function.Consumer;
 
 /**
- * A function that handles a {@link Command}.
+ * A function that handles a {@link Message}.
  *
  * @author Philipp Ploder
  * @version 1.0.0
  * @since 1.0.0
  */
 @FunctionalInterface
-public interface CommandHandler extends Consumer<Command> {
+public interface MessageListener extends Consumer<Message> {
 
 }
